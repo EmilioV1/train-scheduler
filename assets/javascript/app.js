6cbc639b64bf51e5f8677d23d027901d0da0db97
Emilio Valladares <vallae.wumbo@gmail.com>
2019-10-24 22:00:55 -0700

    Firebase configuration added and initialized. Most recent firebase.js version updated in the head of the html file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-console.log("I am connected!")
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBxvO1hsYj-flOXCxjn3AljHzDDod-vgMU",
+    authDomain: "train-scheduler-e8080.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e8080.firebaseio.com",
+    projectId: "train-scheduler-e8080",
+    storageBucket: "train-scheduler-e8080.appspot.com",
+    messagingSenderId: "848339193631",
+    appId: "1:848339193631:web:53c3b3ae6e6ad3ba366a2a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
