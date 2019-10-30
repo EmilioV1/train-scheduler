e1d985c1669bdf0cd639c7f3bbc97e5337c8520c
Emilio Valladares <vallae.wumbo@gmail.com>
2019-10-29 19:17:44 -0700

    Javascript complete and firebase config information added. Testing connection to firebase.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,40 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBxvO1hsYj-flOXCxjn3AljHzDDod-vgMU",
-    authDomain: "train-scheduler-e8080.firebaseapp.com",
-    databaseURL: "https://train-scheduler-e8080.firebaseio.com",
-    projectId: "train-scheduler-e8080",
-    storageBucket: "train-scheduler-e8080.appspot.com",
-    messagingSenderId: "848339193631",
-    appId: "1:848339193631:web:53c3b3ae6e6ad3ba366a2a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBxvO1hsYj-flOXCxjn3AljHzDDod-vgMU",
+  authDomain: "train-scheduler-e8080.firebaseapp.com",
+  databaseURL: "https://train-scheduler-e8080.firebaseio.com",
+  projectId: "train-scheduler-e8080",
+  storageBucket: "train-scheduler-e8080.appspot.com",
+  messagingSenderId: "848339193631",
+  appId: "1:848339193631:web:53c3b3ae6e6ad3ba366a2a"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var trainData = firebase.database();
+
+$("#addTrainBtn").on("click", function(){
+  var trainName = $("#trainNameInput").val().trim();
+  var destination = $("#destinationInput").val().trim();
+  var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10,"years").format(x);
+  var frequency = $("#frequencyInput").val().trim();
+  
+  var newTrain = {
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  }
+
+  trainData.ref().push(newTrain);
+
+  alert("Train Added!");
+
+  $("#trainNameInput").val("");
+  $("destinationInput").val("");
+  $("#firstTrainInput").val("");
+  $("#frequencyInput").val("");
+
+  return false;
+})
