20aa7a2c834ff1ca0333ef7fd9d49f21b1d4f606
Emilio Valladares <vallae.wumbo@gmail.com>
2019-11-22 22:16:14 -0800

    Added js to pull from db.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,3 +37,18 @@ $("#addTrainBtn").on("click", function(){
 
   return false;
 })
+
+trainData.ref().on("child_added", function(snapshot){
+  var name = snapshot.val().name;
+  var destination = snapshot.val().destination;
+  var frequency = snapshot.val().frequency;
+  var firstTrain = snapshot.val().firstTrain;
+
+  var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
+  var minutes = frequency - remainder;
+  var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+  console.log(remainder);
+  console.log(minutes);
+  console.log(arrival);
+})
